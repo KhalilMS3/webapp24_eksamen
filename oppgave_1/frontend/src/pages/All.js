929f23e77659eeb6dd9baff3e773b0e3d114c0ec
KhalilMS3 <kmsfouk@hiof.no>
2024-11-09 23:53:30 +0100

    refactor: moved courses page into own component

--- oppgave_1/frontend/src/pages/All.js
+++ oppgave_1/frontend/src/pages/All.js
@@ -117,85 +117,7 @@ function Course() {
 }
 
 function Courses() {
-  const [value, setValue] = useState("");
-  const [data, setData] = useState(courses);
-
-  const handleFilter = (event) => {
-    const category = event.target.value;
-    setValue(category);
-    if (category && category.length > 0) {
-      const content = courses.filter((course) =>
-        course.category.toLocaleLowerCase().includes(category.toLowerCase())
-      );
-      setData(content);
-    } else {
-      setData(courses);
-    }
-  };
-
-  return (
-    <>
-      <header className="mt-8 flex items-center justify-between">
-        <h2 className="mb-6 text-xl font-bold" data-testid="title">
-          Alle kurs
-        </h2>
-        <label className="flex flex-col text-xs font-semibold" htmlFor="filter">
-          <span className="sr-only mb-1 block">Velg kategori:</span>
-          <select
-            id="filter"
-            name="filter"
-            data-testid="filter"
-            value={value}
-            onChange={handleFilter}
-            className="min-w-[200px] rounded bg-slate-200"
-          >
-            <option value="">Alle</option>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </label>
-      </header>
-      <section className="mt-6 grid grid-cols-3 gap-8" data-testid="courses">
-        {data && data.length > 0 ? (
-          data.map((course) => (
-            <article
-              className="rounded-lg border border-slate-400 px-6 py-8"
-              key={course.id}
-              data-testid="course_wrapper"
-            >
-              <span className="block text-right capitalize">
-                [{course.category}]
-              </span>
-              <h3
-                className="mb-2 text-base font-bold"
-                data-testid="courses_title"
-              >
-                <a href={`/kurs/${course.slug}`}>{course.title}</a>
-              </h3>
-              <p
-                className="mb-6 text-base font-light"
-                data-testid="courses_description"
-              >
-                {course.description}
-              </p>
-              <a
-                className="font-semibold underline"
-                data-testid="courses_url"
-                href={`/kurs/${course.slug}`}
-              >
-                Til kurs
-              </a>
-            </article>
-          ))
-        ) : (
-          <p data-testid="empty">Ingen kurs</p>
-        )}
-      </section>
-    </>
-  );
+  
 }
 
 function Lesson() {
@@ -918,30 +840,6 @@ function Create() {
 
 export default function All() {
   return (
-    <div
-      className="mx-auto grid min-h-screen w-full max-w-7xl grid-rows-[auto_minmax(900px,_1fr)_30px]"
-      data-testid="layout"
-    >
-      <nav className="mt-6 mb-12 flex justify-between">
-        <h1 className="text-lg font-bold uppercase" data-testid="logo">
-          <a href="/">Mikro LMS</a>
-        </h1>
-        <ul className="flex gap-8" data-testid="nav">
-          <li className="text-base font-semibold" data-testid="nav_courses">
-            <a href="kurs">Kurs</a>
-          </li>
-          <li className="text-base font-semibold" data-testid="nav_new">
-            <a href="/ny">Nytt kurs</a>
-          </li>
-        </ul>
-      </nav>
-      <main className="h-full">
-        <p>Siden er tom</p>
-      </main>
-      <footer className="flex justify-between" data-testid="footer">
-        <p>Mikro LMS AS, 2024</p>
-        <p>99 00 00 00, mail@lms.no</p>
-      </footer>
-    </div>
+    <p>Hello from All.js</p>
   );
 }
