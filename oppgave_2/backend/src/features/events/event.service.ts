1eafb9049f807479d43071d4c3a489a9679a1da8
KhalilMS3 <kmsfouk@hiof.no>
2024-11-22 22:55:10 +0100

    feat(N-Tier): Service Layer

---
+++ oppgave_2/backend/src/features/events/event.service.ts
@@ -0,0 +1,91 @@
+import { Result } from "@/types";
+import { Event, eventSchema } from "./event.schema";
+import { EventRepository } from "./event.repository";
+import { z } from "zod";
+import { STATUS_CODES } from "@/lib/error";
+import { randomUUID } from "crypto";
+
+
+class createEventService {
+   async listEvents(): Promise<Result<Event[]>>{
+      return await EventRepository.listEvents()
+   }
+   async getEventBySlug(slug: string): Promise<Result<Event>>{
+      if (!slug) {
+         return {
+            success: false,
+            error: {
+               code: STATUS_CODES.BAD_REQUEST,
+               message: "Invalid event slug"
+            }
+         }
+      }
+      return await EventRepository.getEventBySlug(slug)
+   }
+   async createEvent(data: any): Promise<Result<Event>>{
+      try {
+         if (!data.id) {
+            data.id = randomUUID()
+         }
+         const parsedEventData = eventSchema.parse(data)
+         return await EventRepository.createEvent(parsedEventData)
+      } catch (error) {
+         if (error instanceof z.ZodError) {
+            return {
+               success: false,
+               error: {
+                  code: STATUS_CODES.BAD_REQUEST,
+                  message: JSON.stringify(error.errors)
+               }
+            }
+         } else {
+            return {
+               success: false,
+               error: {
+                  code: STATUS_CODES.INTERNAL_SERVER_ERROR,
+                  message:  `Failed to create event: ${error}`
+               }
+            }
+         }
+      }
+   }
+
+   async updateEvent(id: string, data: any): Promise<Result<Event>>{
+      try {
+         const parsedEvenData = eventSchema.parse(data)
+         return await EventRepository.updateEvent(id, parsedEvenData)
+      } catch (error) {
+         if (error instanceof z.ZodError) {
+            return {
+               success: false,
+               error: {
+               code: STATUS_CODES.BAD_REQUEST,
+               message: JSON.stringify(error.errors)
+               }
+            }
+         } else {
+            return {
+               success: false,
+               error: {
+                  code: STATUS_CODES.INTERNAL_SERVER_ERROR,
+                  message: `Failed to update event: ${error}`
+               }
+            }
+         }
+      }
+   }
+   async deleteEvent(slug: string): Promise<Result<null>>{
+      if (!slug) {
+         return {
+            success: false,
+            error: {
+               code: STATUS_CODES.BAD_REQUEST,
+               message: "Invalid event slug"
+            }
+         }
+      }
+      return await EventRepository.deleteEvent(slug)
+   }
+}
+
+export const EventService = new createEventService();
