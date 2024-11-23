ae0fd672f1fa4b4f96466fbfc349b19ec6c061cd
KhalilMS3 <kmsfouk@hiof.no>
2024-11-23 19:27:01 +0100

    feat(Controller): booking controller to handle requests related to bookings

---
+++ oppgave_2/backend/src/features/bookings/booking.controller.ts
@@ -0,0 +1,81 @@
+import { Hono } from "hono";
+import { BookingService } from "./booking.service";
+import { z } from "zod";
+
+
+const BookingController = new Hono()
+
+BookingController.get('/bookings', async (c) => {
+   const result = await BookingService.listBookings()
+
+   if (!result.success) {
+      return c.json({success: false, error: result.error.message},500)
+   }
+
+      return c.json({ success: true, data: result.data }, 200)
+})
+
+BookingController.get('/bookings/:id', async (c) => {
+   const id = c.req.param('id')
+   const result = await BookingService.getBookingById(id)
+
+   if (!result.success) {
+      return c.json({success: false, error: result.error.message},404)
+   }
+
+   return c.json({ success: true, data: result.data }, 200)
+})
+
+BookingController.post('/bookings', async (c) => {
+   try {
+      const body = await c.req.json()
+      console.log("Received booking data:", body)
+      const response = await BookingService.createBooking(body)
+
+      if (!response.success) {
+      return c.json({success: false, error: response.error.message},400)
+      }
+      
+   return c.json({ success: true, data: response.data }, 201)
+   } catch (error: any) {
+      console.error("Error creating booking:", error)
+      if (error instanceof z.ZodError) {
+      return c.json({success: false, error: error.errors},400)
+      }
+      return c.json({ success: false, error: "Failed to create booking" }, 500)
+   }
+})
+
+BookingController.patch('/bookings/:id', async (c) => {
+   try {
+      const id = c.req.param('id')
+      const body = await c.req.json()
+      
+      console.log(`Received update data of booking id: ${id}:`, body)
+
+      const response = await BookingService.updateBooking(id, body)
+      if (!response.success) {
+         return c.json({success: false, error: response.error.message}, 400)
+      }
+
+      return c.json(response.data, 200)
+   } catch (error: any) {
+      console.error("Error updating booking data:", error)
+      if (error instanceof z.ZodError) {
+         return c.json({success: false, error: error.errors}, 400)
+      }
+         return c.json({success: false, error: "Failed to update booking"}, 500)
+   }
+})
+
+BookingController.delete('/bookings/:id', async (c) => {
+   const id = c.req.param('id')
+   const result = await BookingService.deleteBooking(id)
+
+   if (!result.success) {
+      return c.json({success: false, error: result.error.message}, 404)
+   }
+         return c.json({success: true, message: "Booking deleted successfully!"}, 200)
+})
+
+export default BookingController
