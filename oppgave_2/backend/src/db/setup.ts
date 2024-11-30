f43e583b87cc3b9bc1e32f65cce5f0cd6708332b
KhalilMS3 <kmsfouk@hiof.no>
2024-12-01 00:34:24 +0100

    feat: when deleting an event, its bookings and participant are being deleted automatically
    Updated/recreated bookings and participants tables to have auto delete if its reference is deleted
    - if an event is deleted, related bookings and participants will be deleted automatically
    -  if a booking is deleted, related participants will be deleted automatically

--- oppgave_2/backend/src/db/setup.ts
+++ oppgave_2/backend/src/db/setup.ts
@@ -1,8 +1,9 @@
 import { DB } from "./db"
 import { seed } from "./seed"
-import { createTables } from "./tables"
+import { createTables, dropTables } from "./tables"
 
 export const setup = async (db: DB) => {
    await createTables(db)
+   // await dropTables(db)
    // await seed()
 }
--- oppgave_2/backend/src/db/tables.ts
+++ oppgave_2/backend/src/db/tables.ts
@@ -3,19 +3,27 @@ import { type DB } from "./db"
 export const createTables = (db: DB) => {
    try {
       db.exec(`
-         CREATE TABLE templates(
-         id TEXT PRIMARY KEY,
-         title TEXT NOT NULL,
-         description TEXT,
-         date_locked TEXT,
-         no_overlapping_events INTEGER DEFAULT 0,
-         is_private INTEGER DEFAULT 0,
-         capacity INTEGER,
-         price REAL DEFAULT 0,
-         has_waitlist INTEGER DEFAULT 0,
-         created_at TEXT NOT NULL,
-         updated_at TEXT
-      )
+         CREATE TABLE IF NOT EXISTS bookings (
+            id TEXT PRIMARY KEY,
+            event_id TEXT NOT NULL,
+            customer_name TEXT NOT NULL,
+            customer_email TEXT NOT NULL,
+            total_price REAL,
+            status TEXT DEFAULT "Pending",
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+         );
+
+         CREATE TABLE IF NOT EXISTS participants (
+            id TEXT PRIMARY KEY,
+            booking_id TEXT NOT NULL,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            waitlist_status INTEGER DEFAULT 0,
+            created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (booking_id) REFERENCES bookings(id) ON DELETE CASCADE
+         );
          `)
       console.log("Tables created successfully!")
    } catch (error) {
@@ -24,6 +32,12 @@ export const createTables = (db: DB) => {
 }
 
 export const dropTables = (db: DB) => {
+    db.exec("PRAGMA foreign_keys = OFF;");
+   db.exec(`
+      DROP TABLE participants;
+      `)
+    db.exec("PRAGMA foreign_keys = ON;");
+   
    
 }
 
@@ -61,7 +75,7 @@ export const deleteDatabaseContent = (db: DB) => {
             status TEXT DEFAULT "Pending",
             created_at TEXT DEFAULT CURRENT_TIMESTAMP,
             updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (event_id) REFERENCES events(id)
+            FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
          );
 
          CREATE TABLE IF NOT EXISTS participants (
@@ -71,6 +85,20 @@ export const deleteDatabaseContent = (db: DB) => {
             email TEXT NOT NULL,
             waitlist_status INTEGER DEFAULT 0,
             created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (booking_id) REFERENCES bookings(id)
+            FOREIGN KEY (booking_id) REFERENCES bookings(id) ON DELETE CASCADE
          );
+
+         CREATE TABLE templates(
+         id TEXT PRIMARY KEY,
+         title TEXT NOT NULL,
+         description TEXT,
+         date_locked TEXT,
+         no_overlapping_events INTEGER DEFAULT 0,
+         is_private INTEGER DEFAULT 0,
+         capacity INTEGER,
+         price REAL DEFAULT 0,
+         has_waitlist INTEGER DEFAULT 0,
+         created_at TEXT NOT NULL,
+         updated_at TEXT
+      )
 */
