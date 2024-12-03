5ac74395926a6eb715395e2e3e4e0386df5f71d0
KhalilMS3 <kmsfouk@hiof.no>
2024-12-03 17:05:05 +0100

    adjustment(AdminEventDetails): adjusted UI of AdminEventDetails
    AdminEventDetails displays all neccessary information about an event

--- oppgave_2/frontend/src/app/admin/arrangementer/[eventSlug]/page.tsx
+++ oppgave_2/frontend/src/app/admin/arrangementer/[eventSlug]/page.tsx
@@ -1,8 +1,11 @@
 import AdminEventDetails from '@/components/admin/AdminEventDetails'
+import Dashboard from '@/components/admin/Dashboard'
 import React from 'react'
 
 export default function page() {
   return (
+    <Dashboard>
     <AdminEventDetails/>
+    </Dashboard>
   )
 }
--- oppgave_2/frontend/src/components/admin/AdminEventDetails.tsx
+++ oppgave_2/frontend/src/components/admin/AdminEventDetails.tsx
@@ -40,77 +40,106 @@ setSelectedParticipant(null);
 };
 
 return (
-<section className="flex gap-5">
-      
-   <section className="p-10">
-   {eventLoading ? (
-      <p>Laster inn arrangement detaljer...</p>
-   ) : eventError ? (
-      <p>En feil oppstod: {eventError}</p>
-   ) : event ? (
-      <>
-         <h2 className="text-3xl mb-10 font-semibold">{event.title}</h2>
-         <p className="mb-4">
-         Dato: {new Date(event.date).toLocaleDateString()}
-         </p>
-         <p className="mb-4">Sted: {event.location}</p>
-         <p className="mb-4">Type: {event.type}</p>
-         <p className="mb-4">Beskrivelse: {event.description}</p>
-
-         <h3 className="text-2xl mb-6 mt-10 font-semibold">Påmeldinger</h3>
-         {participantsLoading ? (
+  <section className="flex gap-5">
+    <section className="p-10">
+      {eventLoading ? (
+        <p>Laster inn arrangement detaljer...</p>
+      ) : eventError ? (
+        <p>En feil oppstod: {eventError}</p>
+      ) : event ? (
+        <>
+          <h2 className="text-3xl mb-10 font-semibold">
+            Arrangeement: {event.title}
+          </h2>
+          <section className="flex gap-10">
+            <section>
+              <p className="mb-4">
+                <strong>📅Dato:</strong>{" "}
+                {new Date(event.date).toLocaleDateString()}
+              </p>
+              <p className="mb-4">
+                <strong>📍Sted:</strong> {event.location}
+              </p>
+              <p className="mb-4">
+                <strong>🎉Type:</strong> {event.type}
+              </p>
+              <p className="mb-4 ">
+                <strong>📄Beskrivelse:</strong> {event.description}
+              </p>
+              <p className="mb-4">
+                <strong>🔒Privat:</strong> {event.is_private ? "Ja" : "Nei"}
+              </p>
+            </section>
+            <section>
+              <p className="mb-4">
+                <strong>⚙️Status:</strong> {event.status}
+              </p>
+              <p className="mb-4">
+                <strong>🧍🏻Kapasitet:</strong> {event.capacity}
+              </p>
+              <p className="mb-4">
+                <strong>🪑Ledige plasser:</strong> {event.available_spots}
+              </p>
+              <p className="mb-4">
+                <strong>💸Pris:</strong> {event.price},-kr
+              </p>
+              <p className="mb-4">
+                <strong>🕑Venteliste:</strong> {event.waitlist_available ? "Ja" : "Nei"}
+              </p>
+            </section>
+          </section>
+          <h3 className="text-2xl mb-6 mt-10 font-semibold">Påmeldinger</h3>
+          {participantsLoading ? (
             <p>Laster inn påmeldinger...</p>
-         ) : participantsError ? (
+          ) : participantsError ? (
             <p>
-            En feil oppstod ved henting av påmeldinger: {participantsError}
-         </p>
-         ) : participants.length > 0 ? (
+              En feil oppstod ved henting av påmeldinger: {participantsError}
+            </p>
+          ) : participants.length > 0 ? (
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {participants.map((participant: any) => (
-               <ParticipantCard
-               key={participant.id}
-               participant={participant}
-               onEdit={() => handleEditParticipant(participant)}
-               />
-            ))}
-         </section>
-         ) : (
+              {participants.map((participant: any) => (
+                <ParticipantCard
+                  key={participant.id}
+                  participant={participant}
+                  onEdit={() => handleEditParticipant(participant)}
+                />
+              ))}
+            </section>
+          ) : (
             <p>Ingen påmeldinger funnet.</p>
-         )}
-      </>
-   ) : (
-      <p>Ingen data funnet.</p>
-            )}
-               </section>
-            {
-               event && (
-                  <BookingForm
-                     eventId={event.id}
-                     eventPrice={event.price}
-                     available_spots={event.available_spots}
-                     waitlist_available={event.waitlist_available}
-                     is_private={event.is_private}
-                  />
-               )
-            }
-   {selectedParticipant && (
-      <EditParticipantModal
-      participant={selectedParticipant}
-      isOpen={isEditModalOpen}
-      onClose={handleModalClose}
-      onUpdate={(updatedParticipant) => {
-         // Oppdater lokal liste med påmeldinger
-         setLocalParticipants((prev) =>
-            prev?.map((participant: any) =>
-               participant.id === updatedParticipant.id
-         ? updatedParticipant
-               : participant
-            )
-         );
-         }}
-         onDelete={handleDeleteParticipant}
+          )}
+        </>
+      ) : (
+        <p>Ingen data funnet.</p>
+      )}
+    </section>
+    {event && (
+      <BookingForm
+        eventId={event.id}
+        eventPrice={event.price}
+        available_spots={event.available_spots}
+        waitlist_available={event.waitlist_available}
+        is_private={event.is_private}
       />
-)}
-</section>
+    )}
+    {selectedParticipant && (
+      <EditParticipantModal
+        participant={selectedParticipant}
+        isOpen={isEditModalOpen}
+        onClose={handleModalClose}
+        onUpdate={(updatedParticipant) => {
+          // Oppdater lokal liste med påmeldinger
+          setLocalParticipants((prev) =>
+            prev?.map((participant: any) =>
+              participant.id === updatedParticipant.id
+                ? updatedParticipant
+                : participant
+            )
+          );
+        }}
+        onDelete={handleDeleteParticipant}
+      />
+    )}
+  </section>
 );
 }
